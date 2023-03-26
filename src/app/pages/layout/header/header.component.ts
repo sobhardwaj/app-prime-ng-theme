744ff1183866ec4c0dfdb18942553422516df93e
sobhardwaj <siddharathbhardwaj@gmail.com>
2023-03-26 20:47:21 +0530

    searching component list

--- src/app/pages/layout/header/header.component.ts
+++ src/app/pages/layout/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-// import { Event } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, ViewChild } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { fromEvent, Observable, Subject } from 'rxjs';
+import { filter, take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout-header',
@@ -8,8 +10,17 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent {
   gloablSearch: any = ''
-  constructor() {
+  private readonly destroy$ = new Subject<void>()
+  // private readonly document: Document
+  // private lastFocusedElement: HTMLElement
+  showComponentsList: boolean = false
+  showToolbar: boolean = true
+  // components$: Observable<ComponentLink[]> = this.componentsListService.components$;
+  @ViewChild('componentSearch') componentSearch: ElementRef | undefined;
+
+  constructor(private router: Router) {
     console.log(this.gloablSearch)
+
   }
 
   showComponentList() { }
@@ -17,4 +28,25 @@ export class HeaderComponent {
   onSearchChange(event: any) { }
 
   navigateToComponent() { }
+
+
+  // private handleButtonPressUp(e: KeyboardEvent): void {
+  //   if (e.key === 'ArrowDown') {
+  //     this.componentsListService.selectNextComponent();
+  //   }
+
+  //   if (e.key === 'ArrowUp') {
+  //     this.componentsListService.selectPreviousComponent();
+  //   }
+
+  //   if (e.key === 'Escape') {
+  //     this.hideComponentsList();
+  //     this.lastFocusedElement.focus();
+  //   }
+
+  //   if (e.key === '/') {
+  //     this.lastFocusedElement = this.document.activeElement as HTMLElement;
+  //     this.componentSearch.nativeElement.focus();
+  //   }
+  // }
 }
